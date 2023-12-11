690162300d026cbecf6779eef31fc54612e6a931
TBM13 <32852493+TBM13@users.noreply.github.com>
2023-12-11 14:05:05 -0300

    Apply QQ2A.200305.004.A1 OTA (October 20, 2023 build)

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
-if ! applypatch --check EMMC:/dev/block/by-name/recovery:100663296:385dd8796cfaf7b7f79489116039c2114cd23a91; then
+if ! applypatch --check EMMC:/dev/block/by-name/recovery:100663296:7959133e95fd35fd8d0bf081009671497001737c; then
   applypatch  \
           --patch /system/recovery-from-boot.p \
-          --source EMMC:/dev/block/by-name/boot:33554432:a3f47a03ebf6550e6516f59b4f0ee0bd7cca5aa3 \
-          --target EMMC:/dev/block/by-name/recovery:100663296:385dd8796cfaf7b7f79489116039c2114cd23a91 && \
+          --source EMMC:/dev/block/by-name/boot:33554432:7de228094c520d6bb0a99ef62bfb1ed7c6fb9f56 \
+          --target EMMC:/dev/block/by-name/recovery:100663296:7959133e95fd35fd8d0bf081009671497001737c && \
       log -t recovery "Installing new recovery image: succeeded" || \
       log -t recovery "Installing new recovery image: failed"
 else
